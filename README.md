7c9ecccaa66e4c778fb0889d2d7cb2fbc09eb0dc
SirGlaucus <glaucusjoseph@gmail.com>
2022-03-24 22:33:42 -0300

    Desafio terminado, agregando el chart al modal y realizando correciones

---
+++ README.md
@@ -0,0 +1,18 @@
+# Desafio Instafake
+## Para visualizar el siguiente desafio debe descargar los archivos, ejecutar los comandos "npm install" y luego "npm run watch". Por ultimo abrir el enlace http://localhost:3000/covid19/
+
+## Desafio realizado entre el grupo Triple J:
+##### Joseph Ortega
+##### Jossefa Rodriguez
+##### Juan Vega
+
+En este desafio se utilizo:
+- Async/Await
+- Arreglos
+- Fetch
+- IIFE
+- JSON
+- Try/Catch
+- Modificacion de elementos del DOM
+- Boostrap
+- Chart.js
--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,6 +1,6 @@
-const botonPruebaSelector = document.querySelector('#prueba')
-const tablaDatos = document.querySelector('#bodyTabla')
-const formulario = document.querySelector('#js-form')
+(()=>{
+const tablaDatosSelector = document.querySelector('#bodyTabla')
+const formularioSelector = document.querySelector('#js-form')
 const tablaAlbumSelector = document.querySelector('#tabla-album')
 const loginSelector = document.querySelector('#iniciar-sesion')
 const mostrarFormularioSelector = document.querySelector('#mostrar-formulario')
@@ -9,13 +9,13 @@ loginSelector.addEventListener('click', () => {
     mostrarFormularioSelector.setAttribute("style", "display: d-block")
 })
 
-formulario.addEventListener('submit', async (event) => {
+formularioSelector.addEventListener('submit', async (event) => {
     event.preventDefault()
     const email = document.querySelector('#js-input-email').value
     const password = document.querySelector('#js-input-password').value
     const jwt = await postData(email, password)
     getDatosTotales(jwt)
-    getPaises(jwt)
+    getPaisesTabla(jwt)
     mostrarFormularioSelector.setAttribute("style", "display: none")
 })
 
@@ -35,7 +35,7 @@ const postData = async (email, password) => {
     }
 }
 
-const getDatosTotales = async (jwt) => {
+const getDatosTotales = async (jwt) => { // Consumir la API http://localhost:3000/api/total con JavaScript o jQuery.
     try {
         const response = await fetch(`http://localhost:3000/api/total`,
             {
@@ -48,125 +48,41 @@ const getDatosTotales = async (jwt) => {
         const datosFiltrados = data.filter(datosPaises => {
             return datosPaises.active >= 10000
         })
-        console.log(datosFiltrados)
-
-        /*datosFiltrados.forEach(datosPaises => {
-            const casosActivos = datosPaises.active
-            const casosConfirmados = datosPaises.confirmed
-            const casosMuertos = datosPaises.deaths
-            const casosRecuperados = datosPaises.recovered
-            const nombrePais = datosPaises.location
-            console.log(`${casosActivos} - ${casosConfirmados} - ${casosMuertos} - ${casosRecuperados} - ${nombrePais}`)
-        })*/
-
         const labelPaises = datosFiltrados.map(datos => datos.location)
         const dataChartActive = datosFiltrados.map(datos => datos.active)
         const dataChartConfirmed = datosFiltrados.map(datos => datos.confirmed)
         const dataChartRecovered = datosFiltrados.map(datos => datos.recovered)
         const dataChartDeaths = datosFiltrados.map(datos => datos.deaths)
-        console.log(labelPaises)
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labelPaises,
-                datasets: [{
-                    label: 'Activos',
-                    data: dataChartActive,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(255, 99, 132, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(255, 99, 132, 1)'
-                    ],
-                    borderWidth: 1
-                }, {
-                    label: 'Confirmados',
-                    data: dataChartConfirmed,
-                    backgroundColor: [
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(54, 162, 235, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(54, 162, 235, 1)'
-                    ],
-                    borderWidth: 1
-                }, {
-                    label: 'Muertes',
-                    data: dataChartDeaths,
-                    backgroundColor: [
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(255, 206, 86, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(255, 206, 86, 1)'
-                    ],
-                    borderWidth: 1
-                }, {
-                    label: 'Recuperados',
-                    data: dataChartRecovered,
-                    backgroundColor: [
-                        'rgba(46, 204, 113, 0.2)',
-                        'rgba(46, 204, 113, 0.2)',
-                        'rgba(46, 204, 113, 0.2)',
-                        'rgba(46, 204, 113, 0.2)',
-                        'rgba(46, 204, 113, 0.2)',
-                        'rgba(46, 204, 113, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(147, 250, 165, 1)',
-                        'rgba(147, 250, 165, 1)',
-                        'rgba(147, 250, 165, 1)',
-                        'rgba(147, 250, 165, 1)',
-                        'rgba(147, 250, 165, 1)',
-                        'rgba(147, 250, 165, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-        myChart.render()
+        const chartPaginaPrincipal = document.getElementById('myChart')
+        crearChart(labelPaises, dataChartActive, dataChartConfirmed, dataChartDeaths, dataChartRecovered, chartPaginaPrincipal)
+        // Desplegar la información de la API en un gráfico de barra que debe mostrar sólo los países con más de 10000 casos activos.
     } catch (error) {
         console.log(error)
     }
 }
 
-const getPaises = async (jwt) => {
+// continuacion punto 5: para obtener esta información debes llamar a la API
+// http://localhost:3000/api/countries/{country} al momento de levantar el modal.
+window.imprimirDatosPais = async (pais, jwt) => {
+    try {
+        const response = await fetch(`http://localhost:3000/api/countries/${pais}`,
+            {
+                method: 'GET',
+                headers: {
+                    Authorization: `Bearer ${jwt}`
+                }
+            })
+        const { data } = await response.json()
+        $('#exampleModal').modal('toggle')
+        const modalChartSelector = document.querySelector('#myChartModal')
+        crearChartModal(data.location, data.active, data.confirmed, data.recovered, data.deaths, modalChartSelector)
+    }
+    catch (error) {
+        console.log(error)
+    }
+}
+
+const getPaisesTabla = async (jwt) => {
     try {
         tablaAlbumSelector.setAttribute("style", "display: d-block")
         const response = await fetch(`http://localhost:3000/api/total`,
@@ -178,7 +94,7 @@ const getPaises = async (jwt) => {
             })
         const { data } = await response.json()
         if (data) {
-            let rows = '';
+            let rows = ''
             $.each(data, (i, row) => {
                 rows += `<tr>
                             <td> ${row.location} </td>
@@ -186,32 +102,111 @@ const getPaises = async (jwt) => {
                             <td> ${row.deaths} </td>
                             <td> ${row.recovered} </td>
                             <td> ${row.active} </td>
-                            <td> <a href="#" onclick = "imprimirDatosPais("${row.location}", ${jwt})"> Ver Detalle </a> </td>
+                            <td><a href="#" onclick="imprimirDatosPais('${row.location}', '${jwt}')"> Ver Detalle </a></td>
                             
                         </tr>`
-            })
-            tablaDatos.innerHTML = rows
+            }) // Cada fila de la tabla debe incluir un link que diga "ver detalle", al hacer click levante un modal y m
+            // uestre los casos activos, muertos, recuperados y confirmados en un gráfico
+            tablaDatosSelector.innerHTML = rows // Desplegar toda la información de la API en una tabla.
         }
     } catch (error) {
         console.log(error)
     }
-
 }
-window.imprimirDatosPais = async (pais, jwt) => {
-    try {
-        const response = await fetch(`http://localhost:3000/api/countries/${pais}`,
-            {
-                method: 'GET',
-                headers: {
-                    Authorization: `Bearer ${jwt}`
+
+const crearChart = (labelPaises, dataChartActive, dataChartConfirmed, dataChartDeaths, dataChartRecovered, lugarImpresion) => {
+    lugarImpresion.innerHTML = ""
+    const canvas = document.createElement('canvas')
+    lugarImpresion.appendChild(canvas)
+    const ctx = canvas.getContext('2d')
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: labelPaises,
+            datasets: [{
+                label: 'Activos',
+                data: dataChartActive,
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }, {
+                label: 'Confirmados',
+                data: dataChartConfirmed,
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)'
+                ,
+                borderWidth: 1
+            }, {
+                label: 'Muertes',
+                data: dataChartDeaths,
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                borderColor: 'rgba(255, 206, 86, 1)',
+                borderWidth: 1
+            }, {
+                label: 'Recuperados',
+                data: dataChartRecovered,
+                backgroundColor: 'rgba(46, 204, 113, 0.2)',
+                borderColor: 'rgba(147, 250, 165, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
                 }
-            })
-        const { data } = await response.json()
-        console.log(data)
-    }
-    catch (error) {
-        console.log(error)
-
-    }
+            }
+        }
+    })
+    myChart.render()
 }
-// getPaises(localStorage.getItem('jwt-token'))
+
+const crearChartModal = (labelPaises, dataChartActive, dataChartConfirmed, dataChartDeaths, dataChartRecovered, lugarImpresion) => {
+    lugarImpresion.innerHTML = ""
+    const canvas = document.createElement('canvas')
+    lugarImpresion.appendChild(canvas)
+    const ctx = canvas.getContext('2d')
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: [labelPaises],
+            datasets: [{
+                label: 'Activos',
+                data: [dataChartActive],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgba(255, 99, 132, 1)',
+                borderWidth: 1
+            }, {
+                label: 'Confirmados',
+                data: [dataChartConfirmed],
+                backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                borderColor: 'rgba(54, 162, 235, 1)'
+                ,
+                borderWidth: 1
+            }, {
+                label: 'Muertes',
+                data: [dataChartDeaths],
+                backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                borderColor: 'rgba(255, 206, 86, 1)',
+                borderWidth: 1
+            }, {
+                label: 'Recuperados',
+                data: [dataChartRecovered],
+                backgroundColor: 'rgba(46, 204, 113, 0.2)',
+                borderColor: 'rgba(147, 250, 165, 1)',
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    })
+    myChart.render()
+}
+
+// Final funcion IIFE
+})()
