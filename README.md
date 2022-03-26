2928e99d21f87ea4e437a13688fe73f9e5c0121f
SirGlaucus <glaucusjoseph@gmail.com>
2022-03-26 11:03:43 -0300

    Se hicieron comprobaciones para reutilizar una misma tabla

--- README.md
+++ README.md
@@ -1,10 +1,7 @@
-# Desafio Instafake
+# Desafio Covid 19 - Hito 1
 ## Para visualizar el siguiente desafio debe descargar los archivos, ejecutar los comandos "npm install" y luego "npm run watch". Por ultimo abrir el enlace http://localhost:3000/covid19/
 
-## Desafio realizado entre el grupo Triple J:
-##### Joseph Ortega
-##### Jossefa Rodriguez
-##### Juan Vega
+## Desafio realizado entre el grupo Triple J: Joseph Ortega, Jossefa Rodriguez y Juan Vega
 
 En este desafio se utilizo:
 - Async/Await
@@ -16,5 +13,3 @@ En este desafio se utilizo:
 - Modificacion de elementos del DOM
 - Boostrap
 - Chart.js
-
-Hito 1:
--- public/covid19/assets/scripts.js
+++ public/covid19/assets/scripts.js
@@ -1,4 +1,5 @@
 (() => {
+    // ----------------------- Selectores
     const tablaDatosSelector = document.querySelector('#bodyTabla')
     const formularioSelector = document.querySelector('#js-form')
     const tablaAlbumSelector = document.querySelector('#tabla-album')
@@ -6,19 +7,20 @@
     const mostrarFormularioSelector = document.querySelector('#mostrar-formulario')
 
 
-
+    // ----------------------- evento de click en el navbar para mostrar el formulario
     loginSelector.addEventListener('click', () => {
         mostrarFormularioSelector.setAttribute("style", "display: d-block")
     })
 
+    // ----------------------- Evento de submit en el formulario para obtener el JWT
     formularioSelector.addEventListener('submit', async (event) => {
-        event.preventDefault()
+        event.preventDefault() // Necesario para evitar que la pagina se recargue automaticamente
         const email = document.querySelector('#js-input-email').value
         const password = document.querySelector('#js-input-password').value
-        const jwt = await postData(email, password)
+        const jwt = await postData(email, password) // Ejecutamos la funcion post Data que nos retorna el token. Enviamos como  parametros email y contraseña
         getDatosTotales(jwt)
         getPaisesTabla(jwt)
-        mostrarFormularioSelector.setAttribute("style", "display: none")
+        mostrarFormularioSelector.setAttribute("style", "display: none") // Ocultamos el formulario
     })
 
     const postData = async (email, password) => {
@@ -30,7 +32,7 @@
                 })
 
             const { token } = await response.json()
-            localStorage.setItem('jwt-token', token)
+            localStorage.setItem('jwt-token', token) // Luego de generar el token lo guardamos en el locaStorage
             return token
         } catch (error) {
             console.log(`Error: ${error}`)
@@ -49,13 +51,15 @@
             const { data } = await response.json()
             const datosFiltrados = data.filter(datosPaises => {
                 return datosPaises.active >= 10000
-            })
+            }) // Luego de obtener los datos, utilizamos la funcion filter para obtener unicamente los paises con mas de 10mil casos activos
+            // Luego creamos constantes que nos van a permitir devolver solo un tipo de propiedad como un array a nuestro chart
             const labelPaises = datosFiltrados.map(datos => datos.location)
             const dataChartActive = datosFiltrados.map(datos => datos.active)
             const dataChartConfirmed = datosFiltrados.map(datos => datos.confirmed)
             const dataChartRecovered = datosFiltrados.map(datos => datos.recovered)
             const dataChartDeaths = datosFiltrados.map(datos => datos.deaths)
-            const chartPaginaPrincipal = document.getElementById('myChart')
+
+            const chartPaginaPrincipal = document.getElementById('myChart') // La ubicaion que deseamos para nuestro chart de datos de todos los paises
             crearChart(labelPaises, dataChartActive, dataChartConfirmed, dataChartDeaths, dataChartRecovered, chartPaginaPrincipal)
             // Desplegar la información de la API en un gráfico de barra que debe mostrar sólo los países con más de 10000 casos activos.
         } catch (error) {
@@ -66,8 +70,9 @@
     // continuacion punto 5: para obtener esta información debes llamar a la API
     // http://localhost:3000/api/countries/{country} al momento de levantar el modal.
     const imprimirDatosPais = async (e) => {
-        const pais = e.target.dataset.nombre
-        const jwt = localStorage.getItem('jwt-token')
+        const pais = e.target.dataset.nombre // El nombre se guardo como dataset al momento de crear la tabla 
+        // y asignamos eso a una constante para especificar el pais en el fetch
+        const jwt = localStorage.getItem('jwt-token') // Sacamos nuestro token del localStorage para luego hacer el fetch
         try {
             const response = await fetch(`http://localhost:3000/api/countries/${pais}`,
                 {
@@ -79,7 +84,7 @@
             const { data } = await response.json()
             $('#exampleModal').modal('toggle')
             const modalChartSelector = document.querySelector('#myChartModal')
-            crearChartModal(data.location, data.active, data.confirmed, data.recovered, data.deaths, modalChartSelector)
+            crearChart(data.location, data.active, data.confirmed, data.recovered, data.deaths, modalChartSelector)
         }
         catch (error) {
             console.log(error)
@@ -91,16 +96,16 @@
         const td = document.createElement("td")
         td.appendChild(text)
         return td
-    }
+    } // Funcion para crear un td y agregarle un nodo de texto
 
     const crearTr = () => {
-        return document.createElement("tr")
-    }
+        return document.createElement("tr") 
+    } // Funcion para crear un Tr, luego sera usado mas adelante para ingresarle los td
 
 
     const getPaisesTabla = async (jwt) => {
         try {
-            tablaAlbumSelector.setAttribute("style", "display: d-block")
+            tablaAlbumSelector.setAttribute("style", "display: d-block") // La tabla que estaba previamente oculta sera mostrada con los datos
             const response = await fetch(`http://localhost:3000/api/total`,
                 {
                     method: 'GET',
@@ -109,7 +114,9 @@
                     }
                 })
             const { data } = await response.json()
+
             if (data) {
+                // En data tenemos un array de objetos, utilizamos esto con un ciclo for para comenzar a crear las tablas por cada uno de esos datos
                 for (let i = 0; i < data.length; i++) {
                     const tr = crearTr()
                     tr.appendChild(crearTd(data[i].location))
@@ -120,8 +127,8 @@
 
                     const tdA = crearTd('')
                     const detallesEnlace = document.createElement('button')
-                    detallesEnlace.dataset.nombre = data[i].location
-                    detallesEnlace.addEventListener('click', imprimirDatosPais) // detallesEnlace.addEventListener('click', imprimirDatosPais(data[i].location, jwt))
+                    detallesEnlace.dataset.nombre = data[i].location // Le agregamos a nuestro boton un dataset llamado nombre para guardar el string de location
+                    detallesEnlace.addEventListener('click', imprimirDatosPais) // A cada boton le vamos a asignar el evento click y la funcion imprimirDatosPais
                     detallesEnlace.classList.add("btn", "btn-link");
 
                     const detallesEnlaceTexto = document.createTextNode('Ver detalles')
@@ -143,6 +150,16 @@
     }
 
     const crearChart = (labelPaises, dataChartActive, dataChartConfirmed, dataChartDeaths, dataChartRecovered, lugarImpresion) => {
+
+        const isArray = Array.isArray(labelPaises) // Nos sirve para comprobar si tenemos un array o no
+        // En el caso de que no sea un array, devolvemos el valor unico en un array
+        // Esto nos permite reutilizar la tabla
+        const labels = isArray ? labelPaises : [labelPaises]
+        const dataActive = isArray ? dataChartActive : [dataChartActive]
+        const dataConfirmed = isArray ? dataChartConfirmed : [dataChartConfirmed]
+        const dataDeaths = isArray ? dataChartDeaths : [dataChartDeaths]
+        const dataRecovered = isArray ? dataChartRecovered : [dataChartRecovered]
+
         lugarImpresion.innerHTML = ""
         const canvas = document.createElement('canvas')
         lugarImpresion.appendChild(canvas)
@@ -150,29 +167,29 @@
         const myChart = new Chart(ctx, {
             type: 'bar',
             data: {
-                labels: labelPaises,
+                labels: labels,
                 datasets: [{
                     label: 'Activos',
-                    data: dataChartActive,
+                    data: dataActive,
                     backgroundColor: 'rgba(255, 99, 132, 0.2)',
                     borderColor: 'rgba(255, 99, 132, 1)',
                     borderWidth: 1
                 }, {
                     label: 'Confirmados',
-                    data: dataChartConfirmed,
+                    data: dataConfirmed,
                     backgroundColor: 'rgba(54, 162, 235, 0.2)',
                     borderColor: 'rgba(54, 162, 235, 1)'
                     ,
                     borderWidth: 1
                 }, {
                     label: 'Muertes',
-                    data: dataChartDeaths,
+                    data: dataDeaths,
                     backgroundColor: 'rgba(255, 206, 86, 0.2)',
                     borderColor: 'rgba(255, 206, 86, 1)',
                     borderWidth: 1
                 }, {
                     label: 'Recuperados',
-                    data: dataChartRecovered,
+                    data: dataRecovered,
                     backgroundColor: 'rgba(46, 204, 113, 0.2)',
                     borderColor: 'rgba(147, 250, 165, 1)',
                     borderWidth: 1
@@ -189,52 +206,6 @@
         myChart.render()
     }
 
-    const crearChartModal = (labelPaises, dataChartActive, dataChartConfirmed, dataChartDeaths, dataChartRecovered, lugarImpresion) => {
-        lugarImpresion.innerHTML = ""
-        const canvas = document.createElement('canvas')
-        lugarImpresion.appendChild(canvas)
-        const ctx = canvas.getContext('2d')
-        const myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: [labelPaises],
-                datasets: [{
-                    label: 'Activos',
-                    data: [dataChartActive],
-                    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-                    borderColor: 'rgba(255, 99, 132, 1)',
-                    borderWidth: 1
-                }, {
-                    label: 'Confirmados',
-                    data: [dataChartConfirmed],
-                    backgroundColor: 'rgba(54, 162, 235, 0.2)',
-                    borderColor: 'rgba(54, 162, 235, 1)'
-                    ,
-                    borderWidth: 1
-                }, {
-                    label: 'Muertes',
-                    data: [dataChartDeaths],
-                    backgroundColor: 'rgba(255, 206, 86, 0.2)',
-                    borderColor: 'rgba(255, 206, 86, 1)',
-                    borderWidth: 1
-                }, {
-                    label: 'Recuperados',
-                    data: [dataChartRecovered],
-                    backgroundColor: 'rgba(46, 204, 113, 0.2)',
-                    borderColor: 'rgba(147, 250, 165, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        })
-        myChart.render()
-    }
     const autoIniciar = () => {
         jwt = localStorage.getItem('jwt-token')
 
@@ -243,7 +214,7 @@
             getPaisesTabla(jwt)
         }
     }
-    autoIniciar()
+    autoIniciar() // Si tenemos un token en nuestro local storage, ejecutamos nuestras funciones principales
     // Final funcion IIFE
 })()
 
